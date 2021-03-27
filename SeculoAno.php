77ca9494ac730307177e12084df1653b739fa603
YURI F S FREITAS <iruytj@gmail.com>
2021-03-27 14:38:51 -0300

    Agora como função

--- SeculoAno.php
+++ SeculoAno.php
@@ -1,17 +1,26 @@
 <?php
-$ano = $_GET['data'];
 
-//print $ano;
-if($ano > 0){
+if (isset($_POST['ano'])) {
+    seculoAno();
+  }
+  
+function seculoAno(){
+ 	$ano = $_POST['ano'];
+
+    if($ano > 0){
 $seculo = 0;
+
 	if($ano <= 100) {
 		$seculo = 1;
-	}else
-	if($ano >100){
+	}
+	else if($ano >100){
 		if($ano % 100 >= 1){$seculo = round($ano/100) + 1;}
 		else {$seculo = round($ano/100);}
 	}
-print "<h1>Século = {$seculo}</h1>";
 }
+
+	echo "O seculo é ". $seculo .".";
+}
+
 
 ?>
